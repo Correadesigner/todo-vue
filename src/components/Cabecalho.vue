1816e0330b596d8e3ba4e6cbd054b9809e475943
Cleiton Corrêa <cleitonbc7@gmail.com>
2025-08-20 19:44:39 +1000

    componentizando a aplicação

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,13 @@
+<script setup>
+    const props = defineProps(['tarefasPendentes'])
+
+</script>
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light roundend-3">
+        <h1>Minhas Tarefas</h1>
+        <!-- O Filtro abaixo pode ser colocado para contar a quantidade de produtos com aquela nomenclatura -->
+        <p>
+            Você possui {{ props.tarefasPendentes }} tarefas pendentes
+        </p>
+        </header>
+</template>
